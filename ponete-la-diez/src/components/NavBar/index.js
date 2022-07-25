c4324efd58a562283be4ccf65b3e5230f18e1b3b
maxieffron <mleffron86@gmail.com>
2022-07-25 01:42:38 -0300

    Se agrega barra de navegación al proyecto

---
+++ ponete-la-diez/src/components/NavBar/index.js
@@ -0,0 +1,41 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "./NavBar.css";
+import logo from "./logo.png";
+
+//Componente
+const LinkMenu = (props) => {
+    return (
+        <a href={props.href} className="nav-Items">
+            {props.itemMenu}
+        </a>
+    );
+};
+
+function NavBar() {
+    return (
+        <Navbar id="navegation-Bar" expand="lg">
+            <Container id="nav-Container">
+                <Navbar.Brand href="#home">
+                    <img src={logo} className="nav-Logo" alt="logo"></img>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="nav-Menu">
+                        <LinkMenu href={"#home"} itemMenu={"Inicio"} />
+                        <LinkMenu href={"#link"} itemMenu={"Nosotros"} />
+                        <LinkMenu href={"#link"} itemMenu={"Productos"} />
+                        <LinkMenu
+                            href={"#link"}
+                            itemMenu={"Preguntas Frecuentes"}
+                        />
+                        <LinkMenu href={"#link"} itemMenu={"Contacto"} />
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default NavBar;
